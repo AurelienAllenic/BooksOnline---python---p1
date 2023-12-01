13404b35375462d17419f0f425264efd0810c5fc
AurelienAllenic <aurelien.allenic@gmail.com>
2023-12-01 16:43:09 +0100

    (feat) added functions to get all books from all categories and download every image inside /images folder

---
+++ books.py
@@ -0,0 +1,183 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+import os
+import string
+
+def get_one_product(url):
+    # Effectuer la requête HTTP
+    response = requests.get(url)
+
+    # Analyser le contenu HTML avec BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extraire les données nécessaires
+    product_page_url = url
+    title = soup.find('h1')
+    if title:
+        title = title.text
+
+    # Trouver la table contenant les informations
+    table = soup.find('table', class_='table-striped')
+    if table:
+        # Extraire les données de la table
+        upc = table.find('th', string='UPC').find_next_sibling('td')
+        price_including_tax = table.find('th', string='Price (incl. tax)').find_next_sibling('td')
+        price_excluding_tax = table.find('th', string='Price (excl. tax)').find_next_sibling('td')
+        number_available = table.find('th', string='Availability').find_next_sibling('td')
+        if upc and price_including_tax and price_excluding_tax and number_available:
+            upc = upc.text
+            price_including_tax = price_including_tax.text.replace('Â', '')
+            price_excluding_tax = price_excluding_tax.text.replace('Â', '')
+            number_available = number_available.text
+
+            product_description = soup.find('meta', attrs={'name': 'description'})
+            if product_description:
+                product_description = product_description['content']
+
+            # Extraire la catégorie
+            category = soup.find('ul', class_='breadcrumb').find_all('li')[-2].text.strip()
+
+            # Extraire la note de l'évaluation
+            star_rating_element = soup.find('p', class_=re.compile('^star-rating'))
+            classes = star_rating_element.get('class', [])
+            rating = 0
+            if 'One' in classes:
+                rating = 1
+            elif 'Two' in classes:
+                rating = 2
+            elif 'Three' in classes:
+                rating = 3
+            elif 'Four' in classes:
+                rating = 4
+            elif 'Five' in classes:
+                rating = 5
+
+            # Extraire l'URL de l'image
+            image_url = soup.find('div', class_='item active').find('img')['src']
+
+            # Données à écrire dans le fichier CSV
+            data = [
+                f"\nproduct_page_url : {product_page_url}",
+                f"\nuniversal_product_code (upc) : {upc}",
+                f"\ntitle : {title}",
+                f"\nprice_including_tax: {price_including_tax}",
+                f"\nprice_excluding_tax: {price_excluding_tax}",
+                f"\nnumber_available: {number_available}",
+                f"\nproduct_description: {product_description}",
+                f"\ncategory: {category}",
+                f"\nreview_rating : {rating}/5",
+                f"\nimage_url: {image_url}",
+            ]
+
+            return data
+
+def get_from_category(url, category_name):
+    # Effectuer la requête HTTP
+    response = requests.get(url)
+    # Analyser le contenu HTML avec BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Trouver tous les liens des produits sur la page de la catégorie
+    product_links = soup.select('h3 a')
+    counter = 0
+
+    # Liste pour stocker les données de tous les livres de la catégorie
+    category_books_data = []
+
+    # Itérer sur chaque lien de produit et extraire les informations
+    for link in product_links:
+        product_url = link['href']
+        counter += 1
+
+        # Diviser l'url pour récupérer seulement le nom du fichier
+        product_path = product_url.rsplit('/', 2)[-2]
+
+        # Ajouter au nom du fichier le chemin le précédant
+        product_final_url = f"https://books.toscrape.com/catalogue/{product_path}"
+
+        # Appel de la fonction get_one_product        
+        book_data = get_one_product(product_final_url)
+
+        # Ajouter les données du livre à la liste
+        category_books_data.append(book_data)
+
+        # Afficher le lien du produit en console pour vérification
+        print(f"Counter : {counter}")
+
+    # Nom du fichier CSV pour la catégorie
+    csv_file = f'{category_name}_books.csv'
+
+    # Écrire les données dans le fichier CSV
+    with open(csv_file, 'w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+        writer.writerows(category_books_data)
+
+    print(f'Données enregistrées dans {csv_file}')
+
+    return csv_file
+def sanitize_filename(filename):
+    valid_chars = f"-_.() {string.ascii_letters}{string.digits}"
+    return ''.join(c if c in valid_chars else '_' for c in filename)
+
+def download_image(url, category_name, title, base_url='https://books.toscrape.com'):
+    # Assurez-vous que le dossier pour la catégorie existe
+    category_folder = f'images/{category_name}'
+    os.makedirs(category_folder, exist_ok=True)
+
+    # Construire l'URL absolu
+    absolute_url = f'{base_url}/{url}'
+
+    # Extraire le nom de fichier à partir du titre
+    image_name = f'{sanitize_filename(title)}.jpg'
+
+    # Chemin complet du fichier image local
+    local_path = os.path.join(category_folder, image_name)
+
+    # Télécharger l'image
+    response = requests.get(absolute_url)
+    with open(local_path, 'wb') as file:
+        file.write(response.content)
+
+    print(f'Image enregistrée localement : {local_path}')
+
+
+
+def get_all_categories(url):
+    # Effectuer la requête HTTP
+    response = requests.get(url)
+    
+    # Analyser le contenu HTML avec BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Trouver la div contenant les catégories
+    categories_div = soup.find('div', class_='side_categories')
+
+    # Trouver tous les liens à l'intérieur de la div des catégories
+    category_links = categories_div.find_all('a')
+
+    # Récupérer les URLs des catégories
+    category_urls = [url.rsplit('/', 1)[0] + '/' + link['href'] for link in category_links]
+
+    return category_urls
+
+# URL de base
+base_url = 'https://books.toscrape.com/index.html'
+
+# Appeler la fonction avec l'URL de base
+all_categories = get_all_categories(base_url)
+
+# Itérer sur chaque catégorie et appeler la fonction get_from_category
+for category_url in all_categories:
+    category_name = category_url.rsplit('/', 2)[-2]
+    csv_file = get_from_category(category_url, category_name)
+
+    # Itérer sur chaque livre de la catégorie et télécharger l'image
+    with open(csv_file, 'r', newline='', encoding='utf-8-sig') as file:
+        reader = csv.reader(file)
+        next(reader)  # Ignorer l'en-tête
+        for row in reader:
+            image_url = row[-1].split(':')[-1].strip()
+            title = row[2].split(':')[-1].strip()
+            download_image(image_url, category_name, title)
